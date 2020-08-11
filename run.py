4275e9303431eca388d564a5a8e5a50897249d16
swemuel <swemoulson@gmail.com>
2020-08-11 12:02:51 +1200

    w4

--- run.py
+++ run.py
@@ -1,37 +1,82 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
+
+# to run:
+# cd to directory
+# source env/bin/activate
+# export FLASK_APP=run.py;
+# export FLASK_DEBUG=1
+# flask run
+
 MENUDB = 'menu.db'
 
-
-
-
-@app.route('/')
-def index():
-    db = sqlite3.connect(MENUDB)
-
-    burgers= []
-    cur = db.execute('SELECT burger,price FROM burgers')
+def fetchMenu(con):
+    burgers = []
+    free = '0'
+    cur = con.execute('SELECT burger,price FROM burgers WHERE price>=?', (free,))
     for row in cur:
         burgers.append(list(row))
 
-    drinks= []
-    cur = db.execute('SELECT drink,price FROM drinks')
+    drinks = []
+    cur = con.execute('SELECT drink,price FROM drinks')
     for row in cur:
         drinks.append(list(row))
 
-    sides= []
-    cur = db.execute('SELECT sides,price FROM sides')
+    sides = []
+    cur = con.execute('SELECT sides,price FROM sides')
     for row in cur:
         sides.append(list(row))
 
-    db.close()
+    return {'burgers':burgers, 'drinks':drinks, 'sides':sides}
 
+@app.route('/')
+def index():
+    con = sqlite3.connect(MENUDB)
+    menu = fetchMenu(con)
+    con.close()
 
-    return render_template('index.html', disclaimer='may contain traces of nuts', burgers=burgers, drinks=drinks, sides=sides)
-
+    return render_template(
+      'index.html',
+      disclaimer='may contain traces of nuts',
+      burgers=menu['burgers'],
+      drinks=menu['drinks'],
+      sides=menu['sides']
+     )
 
 @app.route('/order')
 def order():
-    return render_template('order.html')
+    con = sqlite3.connect(MENUDB)
+    menu = fetchMenu(con)
+    con.close()
+
+    return render_template(
+      'order.html',
+      disclaimer='may contain traces of nuts',
+      burgers=menu['burgers'],
+      drinks=menu['drinks'],
+      sides=menu['sides']
+     )
+
+@app.route('/confirm', methods=['POST'])
+def confirm():
+    details = {}
+    items = {}
+
+    for input in request.form:
+        if input == 'name' or input == 'address':
+            details[input] = request.form[input]
+        elif request.form[input] and request.form[input]:
+            items[input] = request.form[input]
+
+    con = sqlite3.connect(MENUDB)
+    cur = con.execute(
+        'INSERT INTO orders(name, address, items) VALUES(?,?,?)',
+        (details['name'], details['address'], str(items))
+
+    )
+    con.commit()
+    con.close()
+
+    return render_template('confirm.html', details=details, items=items)
